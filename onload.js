72b08804abb49804347c55036c5ccc136750e222
Jomic <niu541412@gmail.com>
2024-06-25 13:54:44 +0800

    impore compatibility for Firefox

--- onload.js
+++ onload.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     const config = {
-        'backgroundColor': ['#F44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5', '#2196F3', '#03A9F4', '#00BCD4', '#009688', '#4CAF50', '#8BC34A', '#CDDC39', '#FFEB3B', '#FFC107', '#FF9800', '#FF5722', '#795548', '#9E9E9E', '#607D8B'],
-        'opacity': 0.6,
-        'borderRadius': '3px',
-        'bar_width': '6px',
-        'left': '19px',
-        'top': '20px'
+        backgroundColor: ['#F44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5', '#2196F3', '#03A9F4', '#00BCD4', '#009688', '#4CAF50', '#8BC34A', '#CDDC39', '#FFEB3B', '#FFC107', '#FF9800', '#FF5722', '#795548', '#9E9E9E', '#607D8B'],
+        opacity: 0.6,
+        borderRadius: '3px',
+        bar_width: '6px',
+        left: '19px',
+        top: '20px'
     };
 
-    const rand = Math.floor(config['backgroundColor'].length * Math.random());
+    const rand = Math.floor(config.backgroundColor.length * Math.random());
     const timeoutSec = 500;
 
     document.body.insertAdjacentHTML('beforeend', '<div class="wsx_fade"></div><div class="wsx_scroll"><div class="wsx_scroll_bar"></div></div>');
